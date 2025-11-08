b776073ce7f8a5cc0d851fa346bb98652fc205f2
espinalclark <7clarkespinal@gmail.com>
2025-11-08 05:54:19 -0500

    limpiando repositorio

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from database import get_connection
-
-conn = get_connection()
-cur = conn.cursor()
-cur.execute("SELECT DATABASE();")
-print("Conectado a la base de datos:", cur.fetchone()[0])
-conn.close()
-
