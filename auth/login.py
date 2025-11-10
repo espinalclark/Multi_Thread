2d7a84c70e64b4272a5c57c6261236fd03b81c69
espinalclark <7clarkespinal@gmail.com>
2025-11-10 08:56:50 -0500

    subiendo auth

---
+++ auth/login.py
@@ -0,0 +1,18 @@
+from auth.user_manager import get_user_by_username
+from auth.password_utils import check_password
+import bcrypt
+
+class LoginApp:
+    def __init__(self):
+        pass  # No necesitamos la ventana aquí
+
+    def login_user(self, username, password):
+        user_data = get_user_by_username(username)
+        if not user_data:
+            return None, "Usuario no encontrado "
+
+        if check_password(password, user_data[3]):
+            return user_data, None
+        else:
+            return None, "Contraseña incorrecta "
+
