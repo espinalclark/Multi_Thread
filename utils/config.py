4e0b48a5a6cc4a697a8f622c935cfa5bd6734f7d
espinalclark <7clarkespinal@gmail.com>
2025-11-10 08:59:43 -0500

    subiendo utils

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+DATABASE_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "clark",
+    "database": "Multi_Thread",
+    "port": 3306
+}
+
+DOWNLOADS_PATH = "/home/cl4rk/Cybersecurity/Scripting/Python/Multi_Thread/data/downloads"
+TEMP_PATH = f"{DOWNLOADS_PATH}/temp"
+MAX_THREADS = 4  
+
